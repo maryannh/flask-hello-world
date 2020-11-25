6ebde12795a5baaa179c0a6792aa8bed43d9fe67
maryannh <maryann.horley@gmail.com>
2020-11-25 14:42:09 +0000

    Added folders

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Laury!'
 
-# try this https://projects.raspberrypi.org/en/projects/python-web-server-with-flask
+# try this https://projects.raspberrypi.org/en/projects/python-web-server-with-flask. You could try it on your computer first
 
 # the web address is https://code-playground.onrender.com/
 
-# to run the code click the fork icon on the far left, fill in the box with "What did you change?" and click "Commit & push"
+# to run the code click the fork icon on the far left, fill in the box with "What did you change?" and click "Commit & push", give it a few minutes (!) and look at the web page
 
 # do not put any passwords in functions or variables, I'll show you how to do it
 
