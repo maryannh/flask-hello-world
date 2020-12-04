780beb0840c9c907e701adda1e5df4e5ffc45100
maryannh <maryann.horley@gmail.com>
2020-12-04 15:34:01 +0000

    added christmas messae

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
+from datetime import datetime
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+  now = datetime.now()
+  xmas = datetime(now.year, 12, 25) 
+  delta = xmas - now
+  final = delta.days
+  message = "fuck christmas"
+  if final > 0:
+    message = "there are " + final + " days until Christmas"
+  elif final == 0:
+    message = "It's Christmas!" 
+  elif final < 0:
+    message = "it's my birthday soon!"
+  return render_template("index.html" message=message)
 
 # the web address is https://code-playground.onrender.com/
 
