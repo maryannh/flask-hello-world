e7d21b9a621e31b7248e944c248a1a91aa586aa0
maryannh <maryann.horley@gmail.com>
2020-12-04 13:14:59 +0000

    testing out template

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-# try this https://projects.raspberrypi.org/en/projects/python-web-server-with-flask. You could try it on your computer first
-
 # the web address is https://code-playground.onrender.com/
 
 # to run the code click the fork icon on the far left, fill in the box with "What did you change?" and click "Commit & push", give it a few minutes (!) and look at the web page
